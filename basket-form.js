234ab49c69da5d4ad4b3f782a205742cba0fcccc
DaMishalkina <dashaio251@gmail.com>
2020-05-26 19:42:10 +0300

    styling basket form and module for mobile, tablet, pc

--- basket-form.js
+++ basket-form.js
@@ -1,18 +1,6 @@
 const btnBasket = document.querySelector('#basket-button'),
     basketForm = document.querySelector('#basket-form');
 
-// const myFunc = function () {
-//     if(basketForm.style.visibility != 'visible') {
-//         basketForm.style.visibility = 'visible';
-//         document.querySelector('body').classList.add('hide');
-//     } else {
-//         basketForm.style.visibility = 'hidden';
-//         document.querySelector('body').classList.remove('hide');
-//
-//     }
-// };
-//
-// btnBasket.addEventListener('click', myFunc);
 
 btnBasket.onclick = function () {
     if(basketForm.style.visibility != 'visible') {
